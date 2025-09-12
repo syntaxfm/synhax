9c06d3c29ef8e4fa565063cd33b5569e9f09f045
Scott Tolinski <scott.tolinski@gmail.com>
2025-09-11 20:12:21 -0600

    comments out instrumentation

--- src/instrumentation.server.ts
+++ src/instrumentation.server.ts
@@ -1,19 +1,19 @@
-import * as Sentry from '@sentry/sveltekit';
-Sentry.init({
-	dsn: 'https://a825005a7ee52dd8ea15b555f4eaa374@o4507217476845568.ingest.us.sentry.io/4510003969458176',
-	// Adds request headers and IP for users, for more info visit:
-	// https://docs.sentry.io/platforms/javascript/guides/sveltekit/configuration/options/#sendDefaultPii
-	sendDefaultPii: true,
-	//  performance
-	// Set tracesSampleRate to 1.0 to capture 100%
-	// of transactions for tracing.
-	// We recommend adjusting this value in production
-	// Learn more at
-	// https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
-	tracesSampleRate: 1.0,
-	//  performance
-	//  logs
-	// Enable logs to be sent to Sentry
-	enableLogs: true
-	//  logs
-});
+// import * as Sentry from '@sentry/sveltekit';
+// Sentry.init({
+// 	dsn: 'https://a825005a7ee52dd8ea15b555f4eaa374@o4507217476845568.ingest.us.sentry.io/4510003969458176',
+// 	// Adds request headers and IP for users, for more info visit:
+// 	// https://docs.sentry.io/platforms/javascript/guides/sveltekit/configuration/options/#sendDefaultPii
+// 	sendDefaultPii: true,
+// 	//  performance
+// 	// Set tracesSampleRate to 1.0 to capture 100%
+// 	// of transactions for tracing.
+// 	// We recommend adjusting this value in production
+// 	// Learn more at
+// 	// https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
+// 	tracesSampleRate: 1.0,
+// 	//  performance
+// 	//  logs
+// 	// Enable logs to be sent to Sentry
+// 	enableLogs: true
+// 	//  logs
+// });
