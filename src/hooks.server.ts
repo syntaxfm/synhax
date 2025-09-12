2212d8635cc793c347025a4d5f945f194491fe02
Scott Tolinski <scott.tolinski@gmail.com>
2025-09-11 20:27:45 -0600

    removes server setnry

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,20 +7,20 @@ import { sequence } from '@sveltejs/kit/hooks';
 
 // Server SDK initialized in instrumentation.server.ts per manual setup docs
 
-Sentry.init({
-	dsn: 'https://a825005a7ee52dd8ea15b555f4eaa374@o4507217476845568.ingest.us.sentry.io/4510003969458176scott-tolinski-projects / synhax-l1',
-	// Adds request headers and IP for users, for more info visit:
-	// https://docs.sentry.io/platforms/javascript/guides/sveltekit/configuration/options/#sendDefaultPii
-	sendDefaultPii: true,
-	// Set tracesSampleRate to 1.0 to capture 100%
-	// of transactions for tracing.
-	// We recommend adjusting this value in production
-	// Learn more at
-	// https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
-	tracesSampleRate: 1.0,
-	// Enable logs to be sent to Sentry
-	enableLogs: true
-});
+// Sentry.init({
+// 	dsn: 'https://a825005a7ee52dd8ea15b555f4eaa374@o4507217476845568.ingest.us.sentry.io/4510003969458176scott-tolinski-projects / synhax-l1',
+// 	// Adds request headers and IP for users, for more info visit:
+// 	// https://docs.sentry.io/platforms/javascript/guides/sveltekit/configuration/options/#sendDefaultPii
+// 	sendDefaultPii: true,
+// 	// Set tracesSampleRate to 1.0 to capture 100%
+// 	// of transactions for tracing.
+// 	// We recommend adjusting this value in production
+// 	// Learn more at
+// 	// https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
+// 	tracesSampleRate: 1.0,
+// 	// Enable logs to be sent to Sentry
+// 	enableLogs: true
+// });
 
 const myErrorHandler = ({
 	error,
