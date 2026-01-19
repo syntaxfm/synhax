b8e658ab33c2ddfb08471062d8f55b68f63a2b80
Scott Tolinski <scott.tolinski@gmail.com>
2026-01-19 15:10:08 -0700

    adds logg

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -84,6 +84,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 			const session = await auth.api.getSession({
 				headers: event.request.headers
 			});
+			console.log(session);
 			if (session) {
 				event.locals.session = session.session;
 				event.locals.user = {
