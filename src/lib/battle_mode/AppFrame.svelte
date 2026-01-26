02b88193d49865db474615d72e517b5483a0d384
Wes Bos <wesbos@gmail.com>
2026-01-26 13:14:41 -0500

    adds disableInspect prop to AppFrame component. Fixes #41

--- src/lib/battle_mode/AppFrame.svelte
+++ src/lib/battle_mode/AppFrame.svelte
@@ -11,9 +11,16 @@
 		iframeElement?: HTMLIFrameElement | null;
 		/** Called when iframe finishes loading new content */
 		onload?: () => void;
+		/** When true, adds an overlay to prevent inspect element */
+		disableInspect?: boolean;
 	}
 
-	let { hax, iframeElement = $bindable(null), onload }: Props = $props();
+	let {
+		hax,
+		iframeElement = $bindable(null),
+		onload,
+		disableInspect = false
+	}: Props = $props();
 	const code = $derived(combine_html_and_css(hax.html, hax.css));
 
 	function handleLoad() {
@@ -26,15 +33,31 @@
 	}
 </script>
 
-<iframe
-	bind:this={iframeElement}
-	srcdoc={code}
-	title="App Frame"
-	onload={handleLoad}
-	sandbox="allow-same-origin allow-scripts"
-></iframe>
+<div class="frame-container" class:disable-inspect={disableInspect}>
+	<iframe
+		bind:this={iframeElement}
+		srcdoc={code}
+		title="App Frame"
+		onload={handleLoad}
+		sandbox="allow-same-origin allow-scripts"
+	></iframe>
+</div>
 
 <style>
+	.frame-container {
+		width: 100%;
+		height: 100%;
+		position: relative;
+	}
+	.frame-container.disable-inspect::before {
+		content: '';
+		position: absolute;
+		inset: 0;
+		z-index: 10;
+		/* Transparent overlay that blocks pointer events to iframe */
+		pointer-events: auto;
+	}
+
 	iframe {
 		width: 100%;
 		height: 100%;
